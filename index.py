36e26b3f6444e862d4fd7f1bfdad329dc351f6b9
Maxen Niedermier <maxenlee123@gmail.com>
2024-03-26 20:40:10 -0600

    Update and rename index.py to app.py

--- index.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask 
-
-helloworld =  Flask(__name__)
-@helloworld.route("/")
-def run():
-    return {"message":"Hey there Maxito"}
-
-if __name__ == "__main__":
-    helloworld.run(host = "0.0.0.0", port = int("3000"), debug = True)
